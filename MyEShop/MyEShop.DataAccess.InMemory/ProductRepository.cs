3cf6cfbb2fd510bd9304671e722334191c315904
leomasg <leomasg@gmail.com>
2022-11-09 12:53:18 -0300

    Product Model and InMemory Product Repository Build

---
+++ MyEShop/MyEShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyEShop.Core.Models;
+
+namespace MyEShop.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products;
+
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+
+        public void Comit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product p )
+        {
+            Product productToUpdate = products.Find(x => x.Id == p.Id);
+            if (productToUpdate != null)
+            {
+                productToUpdate = p;
+            }
+            else
+            {
+                throw new Exception("Product not found !");
+            }
+        }
+
+        public Product Find(string Id)
+        {
+            Product product = products.Find(x => x.Id == Id);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found !");
+            }
+        }
+
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable<Product>();
+        }
+
+        public void Delete(string Id)
+        {
+            Product productToUpdate = products.Find(x => x.Id == Id);
+            if (productToUpdate != null)
+            {
+                products.Remove(productToUpdate);
+            }
+            else
+            {
+                throw new Exception("Product not found, !");
+            }
+        }
+
+
+    }
+}
---
+++ MyEShop/MyEShopy.Core/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEShop.Core.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        [StringLength(20)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+        public string description { get; set; }
+        [Range(0,1000)]
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+        public Product(string name, string description, decimal price, string category, string image)
+        {
+            this.Id = this.Id = Guid.NewGuid().ToString();
+            this.Name = name;
+            this.description = description;
+            this.Price = price;
+            this.Category = category;
+            this.Image = image;
+        }
+    }
+}
