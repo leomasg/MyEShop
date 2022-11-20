9ed19c022e6ab9a08d98fbf45f21230d93c11618
leomasg <leomasg@gmail.com>
2022-11-20 18:55:16 -0300

    Created Product Filter

--- MyEShop/MyEShop.WebUi/Controllers/HomeController.cs
+++ MyEShop/MyEShop.WebUi/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyEShop.Core.Contracts;
 using MyEShop.Core.Models;
+using MyEShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,10 +21,25 @@ namespace MyEShop.WebUi.Controllers
         }
 
 
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            //List<Product> products = context.Collection().ToList();
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+
+            if (Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+
+            return View(model);
         }
 
         public ActionResult Details(string Id)
